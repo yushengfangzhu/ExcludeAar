0918fe7cc7fd0345a863618d6c949e42ac7840b3
yepeng <yepeng@leadeon.cn>
2022-04-02 17:45:39 +0800

    说明

---
+++ app/src/main/java/MainClass.java
@@ -0,0 +1,11 @@
+/**
+ * Author by YEPENG, Date on 2022/4/2.
+ */
+public class MainClass {
+
+//    String  origin='tv.danmaku.ijk.media.player.misc.IjkTrackInfo'
+//            'tv.danmaku.ijk.media.player.pragma.DebugLog'
+//            'tv.danmaku.ijk.media.player.pragma.Pragma'
+//            "\n" +
+//            "Go to the documentation to learn how to Fix dependency resolution errors.\n";
+}
